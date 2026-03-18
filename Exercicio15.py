2c3d3e08e525425f5c1be83da19077c33280a440
CGomesLucas <cruzlucasads1@gmail.com>
2026-03-17 23:27:36 -0300

    Exerc 15]

---
+++ Exercicio15.py
@@ -0,0 +1,6 @@
+qtd_dias_projeto = int(input("Digite a quantidade de dias do projeto:"))
+horas_por_dia = int(input("Digite a quantidade de horas trabalhadas por dia:"))
+
+resultado = paginas_por_minuto * tempo_em_minutos
+
+print(f"O total de páginas impressas é: {resultado} páginas")
