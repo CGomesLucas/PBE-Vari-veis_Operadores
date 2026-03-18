2882306e3628893902fa689d74fae7eea195127a
CGomesLucas <cruzlucasads1@gmail.com>
2026-03-17 23:48:32 -0300

    Exerc 20

---
+++ Exercicio20.py
@@ -0,0 +1,6 @@
+base = float(input("Digite o valor base: "))
+expoente = float(input("Digite o expoente: "))
+
+resultado = base ** expoente
+
+print(f"O resultado de {base} elevado a {expoente} é: {resultado}")
