f4e793602ecbca34de5bd59b7ce3841049924e4c
CGomesLucas <cruzlucasads1@gmail.com>
2026-03-17 23:47:28 -0300

    Exerc 18

---
+++ Exercicio18.py
@@ -0,0 +1,6 @@
+acessos_por_minuto = int(input("Digite os acessos por minuto: "))
+minutos = int(input("Digite a quantidade de minutos: "))
+
+total_acessos = acessos_por_minuto * minutos
+
+print(f"O total de acessos registrados foi: {total_acessos}")
