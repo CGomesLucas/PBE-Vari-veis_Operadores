de6e014006c4464b4a4c44b3037ac04d89250fad
CGomesLucas <cruzlucasads1@gmail.com>
2026-03-17 23:16:24 -0300

    Exerc 12

---
+++ Exercicio12.py
@@ -0,0 +1,6 @@
+comprimento = int(input("Digite o comprimento do cabo: "))
+qtd_cabos_comprados = int(input("Digite o quantidade de cabos comprados: "))
+
+resultado = comprimento * qtd_cabos_comprados
+
+print(f"O comprimento total dos cabos intalados é {resultado} metros")
