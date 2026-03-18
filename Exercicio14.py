cf280fd7e9b2dcf7cbdd446eac9901754ab88fe2
CGomesLucas <cruzlucasads1@gmail.com>
2026-03-17 23:24:09 -0300

    Exerc 14

---
+++ Exercicio14.py
@@ -0,0 +1,6 @@
+paginas_por_minuto = int(input("Digite a quantidade de páginas impressas por minuto:"))
+tempo_em_minutos = int(input("Digite a tempo da impressão em minutos:"))
+
+resultado = paginas_por_minuto * tempo_em_minutos
+
+print(f"O total de páginas impressas é: {resultado} páginas")
