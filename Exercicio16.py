3e8d368d93efd0125cf0de940ad632c5df9cdacb
CGomesLucas <cruzlucasads1@gmail.com>
2026-03-17 23:33:11 -0300

    Exerc 16

---
+++ Exercicio16.py
@@ -0,0 +1,6 @@
+consumo_energia = int(input("Digite o consumo de energia do servidor em watts:"))
+quantidade_servidores = int(input("Digite a quantidade de servidores:"))
+
+resultado = consumo_energia * quantidade_servidores
+
+print(f"O consumo total de energia é {resultado} watts")
