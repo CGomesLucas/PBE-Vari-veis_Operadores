20030305814a537327c8e985a4a56f7a3244846e
CGomesLucas <cruzlucasads1@gmail.com>
2026-03-17 23:43:38 -0300

    Exerc 17

---
+++ Exercicio17.py
@@ -0,0 +1,6 @@
+capacidade_HD = int(input("Digite a capacidade do HD em TB (Terabytes) em watts:"))
+quantidade_HDs = int(input("Digite a quantidade de HDs:"))
+
+resultado = capacidade_HD * quantidade_HDs
+
+print(f"A capacidade total de armazenamento é {resultado} TB")
